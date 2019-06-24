703158a4f862d0d3351ae5a37a5c1925ad69e569
ljx@botsally.com <ljx@botsally.com>
2019-06-24 19:04:43 +0800

    修改

--- demo_1.py
+++ demo_1.py
@@ -6,8 +6,13 @@ fashion_minist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_minist.load_data()
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-plt.figure()
-plt.imshow(train_images[0])
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+plt.imshow(train_images[1])
 plt.colorbar()
 plt.grid(False)
 plt.show()
