8d5818b9c9afd9f66455bd09e9e701f5933e5014
ljx@botsally.com <ljx@botsally.com>
2019-06-27 18:58:17 +0800

    创建新的tensorflow

--- demo_1.py
+++ demo_1.py
@@ -62,10 +62,21 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[true_label].set_color('blue')
 
 
-i = 0
-plt.figure(figsize=(6, 3))
-plt.subplot(1, 2, 1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1, 2, 2)
-plot_value_array(i, predictions, test_labels)
+# i = 0
+# plt.figure(figsize=(6, 3))
+# plt.subplot(1, 2, 1)
+# plot_image(i, predictions, test_labels, test_images)
+# plt.subplot(1, 2, 2)
+# plot_value_array(i, predictions, test_labels)
+# plt.show()
+
+num_rows = 5
+num_cols = 3
+num_images = num_cols * num_rows
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, num_cols * 2, 2 * i + 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions, test_labels)
 plt.show()
