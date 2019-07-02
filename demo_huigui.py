e1bce18f36a9913ef9dfe8298ff3640460519733
ljx@botsally.com <ljx@botsally.com>
2019-07-02 19:07:32 +0800

    修改

--- demo_huigui.py
+++ demo_huigui.py
@@ -4,3 +4,20 @@ import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+dataset_path = keras.utils.get_file('auto-mpg.data',
+                                    'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data')
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(dataset_path, names=column_names, na_values='?', comment='\t', sep=' ', skipinitialspace=True)
+dataset = raw_dataset.copy()
+dataset.tail()
+dataset.isna().sum()
+
+origin = dataset.pop('Origin')
+dataset['USA'] = (origin == 1) * 1.0
+dataset['Europe'] = (origin == 2) * 1.0
+dataset['Japan'] = (origin == 3) * 1.0
+train_dataset=dataset.sample(frac=0.8,random_state=0)
+test_dataset=dataset.drop(train_dataset.index)
+sns.pairplot(train_dataset[["MPG", "Cylinders", "Displacement", "Weight"]],diag_kws='kde')
