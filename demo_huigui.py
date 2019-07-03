dd6914732457b573892c6b0f4514cbb9c83c2d10
ljx@botsally.com <ljx@botsally.com>
2019-07-03 16:36:15 +0800

    修改

--- demo_huigui.py
+++ demo_huigui.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
@@ -18,6 +19,45 @@ origin = dataset.pop('Origin')
 dataset['USA'] = (origin == 1) * 1.0
 dataset['Europe'] = (origin == 2) * 1.0
 dataset['Japan'] = (origin == 3) * 1.0
-train_dataset=dataset.sample(frac=0.8,random_state=0)
-test_dataset=dataset.drop(train_dataset.index)
-sns.pairplot(train_dataset[["MPG", "Cylinders", "Displacement", "Weight"]],diag_kws='kde')
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+sns.pairplot(train_dataset[["MPG", "Cylinders", "Displacement", "Weight"]], diag_kind='kde')
+train_stas = train_dataset.describe()
+train_stas.pop('MPG')
+train_stas = train_stas.transpose()
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+
+
+def norm(x):
+    return (x - train_stas['mean']) / train_stas['std']
+
+
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+
+def build_model():
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation=tf.nn.relu, input_shape=([len(train_dataset.keys())])),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dense(1)
+    ])
+
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
+    return model
+
+
+example_data = normed_train_data[:10]
+model = build_model()
+result=model.predict(example_data)
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0: print('')
+        print('.', end='')
+
+
+Epoch = 1000
+history = model.fit(normed_train_data, train_labels, epochs=Epoch, verbose=0, validation_split=0.2,callbacks=[PrintDot()])
+print(history.history)
